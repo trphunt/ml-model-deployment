a5e833d37254ee5fc97fc8e0486a0a90031b21ac
Tyler Hunt <tyler.rp.hunt@gmail.com>
2021-03-06 04:07:49 -0500

    add knn classifier files

---
+++ python_hello_world.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 02:42:58 2021
+
+@author: repair
+"""
+
+print('Hello world')
---
+++ use_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 03:40:55 2021
+
+@author: repair
+"""
+
