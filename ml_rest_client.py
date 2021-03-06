803aeba9c899775626fee73eb94701a28f6a22ed
Tyler Hunt <tyler.rp.hunt@gmail.com>
2021-03-06 09:44:24 -0500

    add customermodel.zip

--- ml_rest_client.py
+++ ml_rest_client.py
@@ -1,7 +1,8 @@
 import json
 import requests
 
-url = 'http://42269d1f2d9c.ngrok.io/predict'
+#url = 'http://42269d1f2d9c.ngrok.io/predict'
+url = 'http://35.226.190.29:8005/model'
 
 request_data = json.dumps({'age':42, 'salary':50000})
 response = requests.post(url, request_data)
